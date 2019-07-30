7f0aa0d96a64d75e3a5d08cc0c123da4211cabc3
jack.ren <fcrj163@sina.com>
2019-07-30 11:14:45 +0800

    增加了AI，优惠券推荐

--- src/main/java/com/yygood/eurekaserver/EurekaserverApplication.java
+++ src/main/java/com/yygood/eurekaserver/EurekaserverApplication.java
@@ -11,6 +11,8 @@ public class EurekaserverApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaserverApplication.class, args);
         System.out.println("增加MA功能");
+
+        System.out.println("增加了AI功能，优惠券推荐");
     }
 
 }

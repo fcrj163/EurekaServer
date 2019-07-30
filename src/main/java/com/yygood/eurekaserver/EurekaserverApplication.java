0f2343456dde7e71385aeeaf968d983a89f6a804
jack.ren <fcrj163@sina.com>
2019-07-30 11:08:18 +0800

    格式化代码20190730

--- src/main/java/com/yygood/eurekaserver/EurekaserverApplication.java
+++ src/main/java/com/yygood/eurekaserver/EurekaserverApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaserverApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(EurekaserverApplication.class, args);
         System.out.println("增加MA功能");
     }

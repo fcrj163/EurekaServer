3663cc7d8157f4638a99f9595bd34ba5b8149328
jack.ren <jack.ren@chinapex.com>
2020-03-25 15:45:37 +0800

    20200410

--- src/main/java/com/yygood/eurekaserver/Test04.java
+++ src/main/java/com/yygood/eurekaserver/Test04.java
@@ -1,4 +1,7 @@
 package com.yygood.eurekaserver;
 
 public class Test04 {
+    public static void main(String[] aa){
+
+    }
 }
---
+++ src/main/java/com/yygood/eurekaserver/test05.java
@@ -0,0 +1,4 @@
+package com.yygood.eurekaserver;
+
+public class test05 {
+}
